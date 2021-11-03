e5d5c6efbb8df431548b21551f625c9e5ce95ca0
zahraMajed <zahra_m.alzawad@hotmail.com>
2021-11-03 18:18:01 +0300

    logic

--- app/src/main/java/com/example/tasktimerapp/Adapter.kt
+++ app/src/main/java/com/example/tasktimerapp/Adapter.kt
@@ -1,12 +1,18 @@
 package com.example.tasktimerapp
 
 import android.content.Intent
+import android.os.SystemClock
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Chronometer
+import android.widget.Toast
 import androidx.recyclerview.widget.RecyclerView
 import com.example.tasktimerapp.RoomDB.TasksTable
 import com.example.tasktimerapp.databinding.ItemRowBinding
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.launch
 
 class Adapter(val activity:ViewTask, val TaskList:List<TasksTable>): RecyclerView.Adapter<Adapter.ItemBinding>() {
     class ItemBinding (val bin :ItemRowBinding):RecyclerView.ViewHolder(bin.root)
@@ -22,15 +28,39 @@ class Adapter(val activity:ViewTask, val TaskList:List<TasksTable>): RecyclerVie
         val TaskDes=TaskList[position].discrption
         val TaskTime=TaskList[position].time
 
+        var running = false
+        var pauseOffset: Long = 0
+        var taskTime=""
+
         holder.bin.apply {
             tvTaskNameRV.text=TaskName
 
             FABtimer.setOnClickListener(){
-                activity.intent= Intent(activity,Timer::class.java)
-                activity.intent.putExtra("taskID", TaskId)
-                activity.startActivity(activity.intent)
+                LL1RV.visibility=View.GONE
+                LL2RV.visibility=View.VISIBLE
+                tvTaskNameTimer.text=TaskName
+                tvTaskDescriptionTimer.text=TaskDes
+                activity.Timer(chronometer)
+
             }//end listener
 
+            startbutton.setOnClickListener {
+                activity.start(chronometer,TaskName)
+            }
+
+
+            pausebutton.setOnClickListener {
+                if (activity.pasuse(chronometer,TaskId))
+                    LL1RV.visibility=View.VISIBLE
+                    LL2RV.visibility=View.GONE
+            }
+
+            LL2RV.setOnClickListener(){
+                LL1RV.visibility=View.VISIBLE
+                LL2RV.visibility=View.GONE
+            }
+
+            //go to summary part
             if (activity.isBtnSumarryClick){
                 activity.btnSummaryTask.visibility= View.INVISIBLE
                 if (TaskTime.isNotEmpty()){
--- app/src/main/java/com/example/tasktimerapp/ViewTask.kt
+++ app/src/main/java/com/example/tasktimerapp/ViewTask.kt
@@ -3,7 +3,11 @@ package com.example.tasktimerapp
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.os.SystemClock
 import android.widget.Button
+import android.widget.Chronometer
+import android.widget.Toast
+import androidx.core.os.bundleOf
 import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
 import com.example.tasktimerapp.RoomDB.TasksDatabase
@@ -21,6 +25,10 @@ class ViewTask : AppCompatActivity() {
     lateinit var tasksDB: TasksDatabase
     //our variables
     var isBtnSumarryClick=false
+    var running = false
+    var pauseOffset: Long = 0
+    var taskTime=""
+    var num = 1
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -53,4 +61,54 @@ class ViewTask : AppCompatActivity() {
         }//end CoroutineScope
     }//end getTasks()
 
-}
+    fun updateTime(id:Int,time:String){
+        CoroutineScope(Dispatchers.IO).launch {
+            tasksDB.getTasksDao().updateTaskTime(id, time)
+        }
+    }//end updateTime()
+
+    ////////////////////////Adapter method//////////////////////////
+
+    fun Timer(chronometer: Chronometer) {
+        chronometer.setFormat("Time: %s")
+        chronometer.setBase(SystemClock.elapsedRealtime());
+        chronometer.onChronometerTickListener =
+            Chronometer.OnChronometerTickListener { chronometer ->
+                if (SystemClock.elapsedRealtime() - chronometer.base >= 10000) {
+                    chronometer.base = SystemClock.elapsedRealtime()
+                }//end startTimer
+            }
+    }//end Timer
+
+    fun pasuse(chronometer: Chronometer, TaskId:Int):Boolean {
+        var ishideLL2RV=false
+        if (num==2){
+            if (running){
+                chronometer.stop();
+                running = false;
+                num--
+                ishideLL2RV=true
+                taskTime=chronometer.text.toString()
+                updateTime(TaskId, taskTime)
+            }//end if
+        }
+        return ishideLL2RV
+    }//end pasuse
+
+    fun start(chronometer: Chronometer, taskName:String){
+        var runningTask = ""
+        if (num == 1) {
+            if (!running) {
+                chronometer.setBase(SystemClock.elapsedRealtime() - pauseOffset);
+                chronometer.start();
+                running = true;
+                num++
+                runningTask = taskName
+            } else {
+                Toast.makeText(applicationContext, "Please finish/stop $runningTask first", Toast.LENGTH_SHORT).show()
+            }
+        }
+    }//end start()
+
+}//end class
+
